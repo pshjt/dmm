cdb277a3ab76a015ff61e5532a9d6e410be48893
FourOhFour <no@mail.here>
2023-10-30 20:44:13 +0100

    Removed legacy build scripts and Visual Studio folder.

--- Script/7zppMSVCBuild/BuildDebug.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-
-rd "..\..\External\7zip-cpp\build\7zpp.dir\Debug" /s /q
-del "..\..\External\7zip-cpp\build\Debug\7zpp.*" /f
-
-set build=Debug
-call _Build.bat
-
-pause
--- Script/wxWidgetsMSVCBuild/BuildDebug.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-
-rd "..\..\External\wxWidgets\build\msw\vc_mswud" /s /q
-rd "..\..\External\wxWidgets\lib\vc_lib\mswud" /s /q
-
-set clean=true
-set build=Debug
-call _Build.bat
